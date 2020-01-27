66acc287c18c894d2fd69e50b8697c42ab04c009
ktvrdinic <karlotvrdinic@gmail.com>
2020-01-27 20:22:38 +0100

    Version 1.0.0

--- client/src/Components/scheduler.jsx
+++ client/src/Components/scheduler.jsx
@@ -15,7 +15,9 @@ import {
 } from "../actions/dateActions";
 import PropTypes from "prop-types";
 
-import "./scheduler.scss";
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
 
 class Scheduler extends React.Component {
   componentDidMount() {
